d54d4299dbaafd7b6e400bde27c82694995b4552
luojinghui <939035243@qq.com>
2015-09-19 20:36:16 +0800

    add main function and main_spec function

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -4,20 +4,19 @@ var main = require('../src/main.js');
 describe('test function then return a biggest discount',function() {
     describe("main()",function() {
 
-        it('if list have 0 book,then output biggest discount is 0', function() {
-            var list = {}
-            expect(main(list)).toBe(4);
+        xit('if list have 0 book,then output biggest discount is 0', function() {
+            var list = []
+            expect(main(bookList)).toBe(0);
         });
 
-        it('if list have 6 book,then output biggest discount is 38', function() {
-            var list = {
-                1 : 2,
-                2 : 1,
-                3 : 1,
-                4 : 1,
-                5 : 1
-            }
-            expect(main(list)).toBe(4);
+        xit('if bookList have 1 book,then output biggest discount is 8', function() {
+            var bookList = [1]
+            expect(main(bookList)).toBe(8);
+        });
+
+        xit('if bookList have 6 book,then output biggest discount is 38', function() {
+            var bookList = [1, 1, 2, 2, 3, 3, 4, 5];
+            expect(main(bookList)).toBe(51.2);
         });
     });
 })
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 'use strict';
 var load = require('./model/fixtures.js');
+var basket = require('./model/basket.js');
 
 function main(bookList) {
-    var loadP = load.loadPromotions();
-    console.log(loadP);
+    // var loadP = load.loadPromotions();
+    // console.log(loadP);
     // return loadP;
+  
 }
 module.exports = main;
